d0c09707ade9da0c2d19da8f69857435f0353c8e
aNaKuWaiTy <iaNaKuWaiTy@aNaKuWaiTys-MacBook-Pro.local>
2016-07-29 06:00:47 +0300

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-Karate Do/Pods-Karate Do-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karate_Do : NSObject
+@end
+@implementation PodsDummy_Pods_Karate_Do
+@end
---
+++ Pods/Target Support Files/Pods-Karate Do/Pods-Karate Do-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Karate_DoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Karate_DoVersionString[];
+
